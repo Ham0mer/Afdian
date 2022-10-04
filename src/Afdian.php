4500ef1e1f64dc426e5179a619de5c499b31e6a1
Hammer <oio_qwq@proton.me>
2022-10-04 20:39:07 +0800

    Update Afdian.php

--- src/Afdian.php
+++ src/Afdian.php
@@ -1,5 +1,11 @@
 <?php
-namespace Ham0er\Afdian;
+/**
+ * 爱发电类
+ * @author : hammer <oio_qwq@proton.me>
+ * Licensed ( https://lsls.me )
+ * Copyright (c) 2022~2099 https://lsls.me All rights reserved.
+ */
+namespace Ham0mer\Afdian;
 
 class Afdian{
     private $userId, $token, $http;
@@ -17,7 +23,7 @@ class Afdian{
     {
         $this->userId = $userId;
         $this->token  = $token;
-        $this->http   = new AfdianHttpRequest();
+        $this->http   = new HttpRequest();
     }
 
     /*
@@ -118,43 +124,9 @@ class Afdian{
      * Name: getAllOrders
      * Desc: 获取所有的订单，每一页都读取一次，因为需要循环查询，请尽量避免频繁调用
      * Return: <Array|String> 如果成功，则返回订单列表数组，失败则返回错误消息，注：本方法不会返回 getOrders 的附加信息，例如 total_page 等
-     * Params:
-     *   Int    $cacheTime 查询结果的缓存失效时间，设置为 0 则表示不缓存，立即进行新的查询
-     *   String $cacheFile 查询结果要缓存到的文件名，默认 order_cache.json，如果需要使用 Redis 进行缓存，可以设定 &redis=服务器地址:端口
      */
     public function getAllOrders($cacheTime = 0, $cacheFile = 'order_cache.json')
     {
-        // 如果缓存失效时间 > 0 则表示使用缓存
-        if($cacheTime > 0) {
-            // 判断开头是否是 &redis=
-            if(substr($cacheFile, 0, 7) == '&redis=') {
-                // 获取 Redis 服务器地址和端口
-                $server = explode(':', substr($cacheFile, 7));
-                if(count($server) >= 2) {
-                    // 开始连接 Redis
-                    $redis  = new Redis();
-                    $redis->connect($server[0], Intval($server[1]));
-                    $result = $redis->get('afdian-orders-cache');
-                    // 如果结果不为空，则返回 Redis 查询结果，否则跳出语句，继续往下执行查询
-                    if($result) {
-                        $result          = json_decode($result, true);
-                        $result['cache'] = 'cached';
-                        return $result;
-                    }
-                } else {
-                    return "Failed to connect redis server: invalid server address";
-                }
-            } else {
-                // 判断文件是否存在，以及文件修改时间是否已超出缓存失效时间
-                if(file_exists($cacheFile) && time() - filemtime($cacheFile) < $cacheTime) {
-                    $result          = json_decode(file_get_contents($cacheFile), true);
-                    $result['cache'] = 'cached';
-                    return $result;
-                }
-            }
-        }
-
-        // 缓存失效或不存在，则开始查询 API
         $orders = ["data" => ["list" => []]];
         $result = $this->getOrders(1);
         if(isset($result['data']['list'], $result['data']['total_page'])) {
@@ -170,19 +142,6 @@ class Afdian{
                 }
             }
         }
-
-        // 判断是否需要缓存
-        if($cacheTime > 0) {
-            // 如果已经定义了 Redis 变量，则表示使用 Redis 作为缓存
-            if(isset($redis)) {
-                $redis->set('afdian-orders-cache', json_encode($orders));
-                $redis->expireAt('afdian-orders-cache', time() + $cacheTime);
-            } else {
-                file_put_contents($cacheFile, json_encode($orders));
-            }
-        }
-
-        $orders['cache'] = 'none';
         return $orders;
     }
 
@@ -190,43 +149,9 @@ class Afdian{
      * Name: getAllSponsors
      * Desc: 获取所有的赞助者，每一页都读取一次，因为需要循环查询，请尽量避免频繁调用
      * Return: <Array|String> 如果成功，则返回赞助者列表数组，失败则返回错误消息，注：本方法不会返回 getSponsors 的附加信息，例如 total_page 等
-     * Params:
-     *   Int    $cacheTime 查询结果的缓存失效时间，设置为 0 则表示不缓存，立即进行新的查询
-     *   String $cacheFile 查询结果要缓存到的文件名，默认 sponsor_cache.json，如果需要使用 Redis 进行缓存，可以设定 &redis=服务器地址:端口
      */
     public function getAllSponsors($cacheTime = 0, $cacheFile = 'sponsor_cache.json')
     {
-        // 如果缓存失效时间 > 0 则表示使用缓存
-        if($cacheTime > 0) {
-            // 判断开头是否是 &redis=
-            if(substr($cacheFile, 0, 7) == '&redis=') {
-                // 获取 Redis 服务器地址和端口
-                $server = explode(':', substr($cacheFile, 7));
-                if(count($server) >= 2) {
-                    // 开始连接 Redis
-                    $redis  = new Redis();
-                    $redis->connect($server[0], Intval($server[1]));
-                    $result = $redis->get('afdian-sponsors-cache');
-                    // 如果结果不为空，则返回 Redis 查询结果，否则跳出语句，继续往下执行查询
-                    if($result) {
-                        $result          = json_decode($result, true);
-                        $result['cache'] = 'cached';
-                        return $result;
-                    }
-                } else {
-                    return "Failed to connect redis server: invalid server address";
-                }
-            } else {
-                // 判断文件是否存在，以及文件修改时间是否已超出缓存失效时间
-                if(file_exists($cacheFile) && time() - filemtime($cacheFile) < $cacheTime) {
-                    $result          = json_decode(file_get_contents($cacheFile), true);
-                    $result['cache'] = 'cached';
-                    return $result;
-                }
-            }
-        }
-
-        // 缓存失效或不存在，则开始查询 API
         $sponsors = ["data" => ["list" => []]];
         $result   = $this->getSponsors(1);
         if(isset($result['data']['list'], $result['data']['total_page'])) {
@@ -242,19 +167,6 @@ class Afdian{
                 }
             }
         }
-
-        // 判断是否需要缓存
-        if($cacheTime > 0) {
-            // 如果已经定义了 Redis 变量，则表示使用 Redis 作为缓存
-            if(isset($redis)) {
-                $redis->set('afdian-sponsors-cache', json_encode($sponsors));
-                $redis->expireAt('afdian-sponsors-cache', time() + $cacheTime);
-            } else {
-                file_put_contents($cacheFile, json_encode($sponsors));
-            }
-        }
-
-        $sponsors['cache'] = 'none';
         return $sponsors;
     }
 
